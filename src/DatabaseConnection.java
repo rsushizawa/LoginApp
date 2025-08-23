c0891bd5510faad98993757aec228a2e1dfb7167
rsushizawa <rodrigo.syu@puccampinas.edu.br>
2025-08-23 10:08:02 -0300

    MySQL Connection

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,3 +1,13 @@
-public class DatabaseConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DatabaseConnection {
+    private static final String url = "jdbc:mysql://localhost:3306/userdb";
+    private static final String username = "root";
+    private static final String password = "12345";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url,username,password);
+    }
 }
